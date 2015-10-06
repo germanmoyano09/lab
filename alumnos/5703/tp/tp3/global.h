2e589d426c26afa9a44896f8a0f5f252a384f884
German Moyano <c.germanmoyano@gmail.com>
2015-10-06 18:55:14 -0300

    Correcciones tp3

--- alumnos/5703/tp/tp3/global.h
+++
@@ -1,6 +0,0 @@
-#ifndef _GLOBAL_H_
-	#define _GLOBAL_H_
-	
-	#define MAX 1024
-
-#endif
--- alumnos/5703/tp/tp3/main.h
+++ alumnos/5703/tp/tp3/main.h
@@ -7,8 +7,16 @@
 	#include <sys/mman.h>
 	#include <string.h>
 	#include <sys/types.h>
-        #include <sys/wait.h>	
+    #include <sys/wait.h>	
+	
+	#define MAX 5120	
 
 	char * replace_filter(char *message, char *filter, char *replace);
+
+	typedef struct {
+	    char buf_est[MAX];
+	    sem_t ph;
+	    sem_t hp;
+	}tp3;
 
 #endif	
